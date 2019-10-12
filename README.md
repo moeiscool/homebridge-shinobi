1954a84440d105dd94dafee3b1e77dd187dd005e
vectronic <hello.vectronic@gmail.com>
2019-10-12 23:35:06 +0100

    feature: initial commit to test!

--- README.md
+++ README.md
@@ -6,28 +6,63 @@
 2. Install this plugin: `npm install -g homebridge-shinobi`
 3. Update your `config.json` configuration file
 
+**NOTE**: Currently there is no support for streaming audio from a camera.
+ 
 # Configuration
 Example `config.json` entry:
 
 ```
 "platforms": [
     {
-        "baseUrl": "http:/x.x.x.x:8080",
+        "shinobiApi": "http:/x.x.x.x:8080",
         "apiKey": "xxx",
-        "groupKey": "yyy",
+        "groupKey": "house",
         "monitors": [
-            "abc",
-            "def"
-        ]
+            "front",
+            "rear"
+        ],
+        "web_hook_port": "8443",
+        "https_key_path": "/cert/privkey.pem",
+        "https_cert_path": "/cert/fullchain.pem"
     }
 ]
 ```
 Where:
 
-* `baseUrl` is the base URL for the [Shinobi API](https://shinobi.video/docs/api)
+* `shinobiApi` is the base URL for the [Shinobi API](https://shinobi.video/docs/api)
 * `apiKey` is a permanent Shinobi API key (configured in the Shinobi dashboard API menu)
 * `groupKey` is a Shinobi group key
 * `monitors` contains a list of Monitor IDs
+* `webhook_port` is the port that the platform should listen on for motion event webhooks from Shinobi
+
+If both `https_key_path` and `https_cert_path` are configured to point at HTTPS key and cert files available on the Homebridge
+server the webhook server will be hosted on HTTPS.
+
+#### Shinobi Integration
+
+The specified `shinobiApi` and `apiKey` will be used to make all Shinobi API requests.
+
+Each of the specified `monitor` IDs will be used with the specified `groupKey` to add a new Homebridge Camera accessory
+to the platform together with a paired Motion Sensor accessory.
+
+When viewing video, the plugin will use the information returned from the API for a specific Monitor to determine
+the source video stream configured. FFmpeg is used to stream directly from the camera and forward to HomeKit. If possible, 
+the video will not be re-encoded.
+
+Snapshot images for each Monitor are simply pulled from the Shinobi API.
+
+A webhook URL should be configured for each Monitor within the Shinobi 'Global Detector Settings' with the following format:
+
+`http[s]://<homebridge_ip>>:<web_hook_port>/mid=<monitors[index]>&group=<groupKey` 
+
+For example, assuming the Homebridge instance is available at 192.168.1.10 and using the above sample config,
+the two Monitors `front` and `rear` would have webhook URLs configured as:  
+
+`https://192.168.1.10:8443/mid=front&group=house` 
+
+and
+
+`https://192.168.1.10:8443/mid=rear&group=house` 
 
 # Help etc.
 
--- index.js
+++ index.js
@@ -1,32 +1,155 @@
 'use strict';
 
+const fetch = require('node-fetch');
+const fs = require('fs');
+const https = require('https');
+const express = require('express');
+
+const ShinobiCameraSource = require('./shonobi_camera_source').ShinobiCameraSource;
+
 let Service, Characteristic, Accessory, hap, UUIDGen;
 
 
-function CameraMotionPlatform(log, config) {
+function ShinobiPlatform(log, config) {
     this.log = log;
     this.config = config;
 
-    if (!this.config) {
-        return;
-    }
+    this.cameraSources = [];
+    this.cameraAccessories = [];
+    this.motionAccessories = [];
 
     this.api.on('didFinishLaunching', this.didFinishLaunching.bind(this));
+    this.api.on('shutdown', this.shutdown.bind(this));
 }
 
 
-CameraMotionPlatform.prototype.accessories = function accessories(callback) {
-    callback();
+ShinobiPlatform.prototype.accessories = function accessories(callback) {
+
+    for (let i = 0; i < this.config.monitors.length; i++) {
+
+        const monitorId = this.config.monitors[i];
+
+        this.motionAccessories[monitorId] = new ShinobiMonitorAccessory(this.log, monitorId);
+    }
+
+    callback(this.motionAccessories);
+
+    const app = express();
+
+    app.get('/', (function(request, response) {
+
+        const monitorId = request.query.mid;
+        const group = request.query.group;
+
+        this.log(`Shinobi motion webhook: group = ${group}, monitorId = ${monitorId}`);
+
+        if ((this.config.groupKey === group) && this.motionAccessories[monitorId]) {
+
+            const motionAccessory = this.motionAccessories[monitorId];
+
+            motionAccessory.setMotion(true);
+
+            setTimeout(motionAccessory.setMotion.bind(motionAccessory), 1000, false);
+
+            response.sendStatus(200);
+        }
+        else {
+            response.sendStatus(400);
+        }
+
+    }).bind(this));
+
+    if (this.config.httpsKeyPath && this.config.httpsCertPath) {
+        const options = {
+            key: fs.readFileSync(this.config.httpsKeyPath),
+            cert: fs.readFileSync(this.config.httpsCertPath)
+        };
+        https.createServer(options, app).listen(this.web_hook_port);
+        this.log(`Started HTTPS server for homebridge-shinobi webhooks on port ${this.web_hook_port}`);
+    }
+    else {
+        app.listen(this.config.web_hook_port);
+        this.log(`Started HTTP server for homebridge-shinobi webhooks on port ${this.web_hook_port}`);
+    }
 };
 
 
-CameraMotionPlatform.prototype.didFinishLaunching = function didFinishLaunching() {
+ShinobiPlatform.prototype.didFinishLaunching = function didFinishLaunching() {
+
+    const promises = [];
+
+    for (let i = 0; i < this.config.monitors.length; i++) {
+
+        const monitorId = this.config.monitors[i];
+        const url = `${this.config.shinobiApi}/${this.config.apiKey}/monitor/${this.config.groupKey}/${monitorId}`;
+
+        const promise = fetch(url)
+            .then(res => res.json())
+            .then(monitorConfig => {
+
+                const uuid = UUIDGen.generate(monitorId);
+
+                const cameraAccessory = new Accessory(monitorId, uuid, hap.Accessory.Categories.CAMERA);
+
+                const cameraSource = new ShinobiCameraSource(hap, this.log, this.config, monitorConfig);
+
+                cameraAccessory.configureCameraSource(cameraSource);
+
+                this.cameraAccessories[monitorId] = cameraAccessory;
+                this.cameraSources[monitorId] = cameraSource;
+            })
+            .catch(err => {
+                this.log(`ShinobiPlatform.didFinishLaunching() error: ${err.message}`);
+            });
+
+        promise.push(promise);
+    }
+
+    Promise.all(promises).then(() => {
+        this.api.publishCameraAccessories('Shinobi', cameraAccessories);
+    })
+    .catch(err => {
+        this.log(`ShinobiPlatform.didFinishLaunching() error: ${err.message}`);
+    });
 };
 
 
-function ShinobiAccessory(log, config) {
+ShinobiPlatform.prototype.shutdown = function shutdown() {
+
+    for (let i = 0; i < this.config.monitors.length; i++) {
+
+        const monitorId = this.config.monitors[i];
+
+        if (this.cameraSources[monitorId]) {
+            this.cameraSources[monitorId].shutdown();
+        }
+    }
+};
+
+
+function ShinobiMonitorAccessory(log, monitorId) {
+
     this.log = log;
+    this.monitorId = monitorId;
+
+    this.motionService = new Service.MotionSensor(this.monitorId);
+
+    this.setMotion(false);
 }
+
+
+ShinobiMonitorAccessory.prototype.setMotion = function setMotion(detected) {
+
+    this.log(`motion: ${this.monitorId} => ${detected}`);
+
+    this.motionService.getCharacteristic(Characteristic.MotionDetected).setValue(detected);
+};
+
+
+ShinobiMonitorAccessory.prototype.getServices = function getServices() {
+
+    return [this.motionService];
+};
 
 
 module.exports = function Shinobi(homebridge) {
@@ -38,6 +161,6 @@ module.exports = function Shinobi(homebridge) {
     UUIDGen = homebridge.hap.uuid;
 
     homebridge.registerPlatform('homebridge-shinobi', 'Shinobi', ShinobiPlatform, true);
-    homebridge.registerAccessory('homebridge-shinobi', 'ShinobiMonitor', ShinobiAccessory);
+    homebridge.registerAccessory('homebridge-shinobi', 'ShinobiMonitor', ShinobiMonitorAccessory);
 };
 
